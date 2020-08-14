84feae7905490e0e866863e07ba572930bd5d12b
HaJooHyun <mangchhe@naver.com>
2020-08-14 22:02:05 +0900

    Nodejs: 라우팅, 랜더링 사용법

--- nodejs/기본 작성법/express.js
+++ nodejs/기본 작성법/express.js
@@ -1,14 +1,26 @@
 /* 
 작성일 : 20/08/13
+라우팅 : 최적의 경로를 찾아주는 것(길잡이 역할), 알맞는 주소로 연결시켜준다.
+랜더링 : 외부 파일의 데이터를 읽어와 html 코드로 만든 다음 클라이언트에게 전달하는 기능
+        -> ejs 모듈 사용
 */
 
 var express = require('express');
 var app = express();
+var controller1 = require('./router/controller1')(app);
+var controller2 = require('./router/controller2')(app);
+var ejs = require('ejs');
 
+app.set('views', __dirname + '/views');
+app.set('view engine', ejs);
+app.engine('html', ejs.renderFile);
+
+app.use(express.static('public'));
+
+/* 
 app.get('/', function (req, res) {
     res.send('ROOT');
 });
-
 app.get('/test1', function (req, res) {
     res.send('TEST1');
 });
@@ -18,6 +30,7 @@ app.get('/test2', function (req, res) {
 app.get('/test3', function (req, res) {
     res.send('TEST3');
 });
+ */
 
 var server = app.listen(2000, function () {
     console.log('서버 가동');
---
+++ nodejs/기본 작성법/public/js/test.js
@@ -0,0 +1,3 @@
+function test_func() {
+    alert('자바 스크립트 코드가 동작하였습니다.');
+}
